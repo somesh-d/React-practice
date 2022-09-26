ba62f195b1829d8f88606c1bdbe83e9d16c24e8e
Somesh Jayant Deshmukh <somesh.deshmukh@yash.com>
2022-09-26 19:20:07 +0530

    backend_server

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const dbConnect = require("./db/dbConnect");
+const bcrypt = require("bcrypt");
+const User = require("./db/userModel");
+
+
+
+// execute database connection 
+dbConnect();
+app.post("/register", (request, response) => {
+    // hash the password
+    bcrypt
+      .hash(request.body.password, 10)
+      .then((hashedPassword) => {
+        // create a new user instance and collect the data
+        const user = new User({
+          email: request.body.email,
+          password: hashedPassword,
+        });
+  
+        // save the new user
+        user
+          .save()
+          // return success if the new user is added to the database successfully
+          .then((result) => {
+            response.status(201).send({
+              message: "User Created Successfully",
+              result,
+            });
+          })
+          // catch error if the new user wasn't added successfully to the database
+          .catch((error) => {
+            response.status(500).send({
+              message: "Error creating user",
+              error,
+            });
+          });
+      })
+      // catch error if the password hash isn't successful
+      .catch((e) => {
+        response.status(500).send({
+          message: "Password was not hashed successfully",
+          e,
+        });
+      });
+  });
+  module.exports = app;
---
+++ db/dbConnect.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+async function dbConnect() {
+  // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
+  mongoose
+    .connect(
+        process.env.DB_URL,
+      {
+        //   these are options to ensure that the connection is done properly
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        
+      }
+    )
+    .then(() => {
+      console.log("Successfully connected to MongoDB Atlas!");
+    })
+    .catch((error) => {
+      console.log("Unable to connect to MongoDB Atlas!");
+      console.error(error);
+    });
+}
+
+module.exports = dbConnect;
