3ec0d3217231c81001eabf900d823ad90105802d
Somesh Jayant Deshmukh <somesh.deshmukh@yash.com>
2022-09-30 17:49:16 +0530

    cors error

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ dbConnect();
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    "Allow-Headers",
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
   );
   res.setHeader(
-    "Allow-Methods",
+    "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
   next();
